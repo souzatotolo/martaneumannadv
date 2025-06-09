56aa72c3e57152abdde175664ef6c4816f198c63
souzatotolo <totolo.adv@gmail.com>
2025-06-09 14:05:31 -0300

    ajustando o contraste

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,19 +6,19 @@ export const Header = () => {
   return <header className="sticky top-0 z-50 bg-primary-600 shadow-sm">
     <div className="container mx-auto px-4 py-3 flex justify-between items-center">
       <div className="flex items-center">
-        <h1 className="text-4xl font-serif font-bold text-gray-600">
-          <span className="text-primary-200">M</span> N
+        <h1 className="text-4xl font-serif font-bold text-gray-100">
+          <span className="text-primary-50">M</span> N
         </h1>
       </div>
       {/* Desktop Navigation */}
       <nav className="hidden md:flex items-center space-x-6">
-        <a href="#sobre" className="text-gray-200 hover:text-gray-600 transition-colors">
+        <a href="#sobre" className="text-gray-100 hover:text-gray-600 transition-colors">
           SOBRE
         </a>
-        <a href="#areas" className="text-gray-200 hover:text-gray-600 transition-colors">
+        <a href="#areas" className="text-gray-100 hover:text-gray-600 transition-colors">
           ÁREAS DE ATUAÇÃO
         </a>
-        <a href="#contato" className="text-gray-200 hover:text-gray-600 transition-colors">
+        <a href="#contato" className="text-gray-100 hover:text-gray-600 transition-colors">
           CONTATO
         </a>
         <a href="https://calendly.com/richardtotolo/agendar-consulta" target='_blank' rel="noopener noreferrer">
@@ -40,14 +40,14 @@ export const Header = () => {
     {/* Mobile Navigation */}
     {isMenuOpen && <nav className="md:hidden bg-primary-600 py-2">
       <div className="container mx-auto px-4 flex flex-col space-y-3">
-        <a href="#sobre" className="text-gray-200 py-2 px-4 hover:bg-gray-50 rounded" onClick={() => setIsMenuOpen(false)}>
+        <a href="#sobre" className="text-gray-50 py-2 px-4 hover:bg-gray-50 rounded" onClick={() => setIsMenuOpen(false)}>
           SOBRE
         </a>
-        <a href="#areas" className="text-gray-200 py-2 px-4 hover:bg-gray-50 rounded" onClick={() => setIsMenuOpen(false)}>
+        <a href="#areas" className="text-gray-50 py-2 px-4 hover:bg-gray-50 rounded" onClick={() => setIsMenuOpen(false)}>
           ÁREAS DE ATUAÇÃO
         </a>
 
-        <a href="#contato" className="text-gray-200 py-2 px-4 hover:bg-gray-50 rounded" onClick={() => setIsMenuOpen(false)}>
+        <a href="#contato" className="text-gray-50 py-2 px-4 hover:bg-gray-50 rounded" onClick={() => setIsMenuOpen(false)}>
           CONTATO
         </a>
         <div className="px-4">
